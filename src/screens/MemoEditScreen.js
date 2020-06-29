d69473e3db5fc940de1bea85af5f0ff354581118
Ibusuki Hajime <hazbon@MacBook-Pro.local>
2020-06-29 18:29:37 +0900

    memoeditscreen create

---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet,View,TextInput} from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <TextInput multiline style={styles.memoeditinput} value="hihi" />
+
+        <CircleButton name="check" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    width:'100%',
+  },
+  memoeditinput:{
+    flex:1,
+    width:'100%',
+    color:'#121212',
+    backgroundColor:'#fefefe',
+    padding:16,
+    paddingTop:32,
+    fontSize:16,
+  },
+});
+
+export default MemoEditScreen;
